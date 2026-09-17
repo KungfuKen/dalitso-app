0c6d237c69e60b91f2b4422b8d6cbf3bb7f01fa6
KungfuKen <61843559+KungfuKen@users.noreply.github.com>
2026-09-17 14:35:14 +0200

    Fix HTML entity encoding in index.js

--- pages/index.js
+++ pages/index.js
@@ -122,7 +122,7 @@ export default function Home() {
             <div className="flex items-stretch justify-center mt-5 md:flex-row flex-col">
 			  
 			 <div className="border border-white m-3 rounded-3xl md:w-1/2 flex flex-col h-100 hover:scale-[1.02] hover:border-green-500 transition-all duration-300">              
-              <div className="relative">    
+              <<div className="relative">    
 				<Image 
                     src="/noor-ouarzazate-city-0384.jpg" 
                     alt="Mesh Network" 
